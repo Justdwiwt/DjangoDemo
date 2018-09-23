efa37d786c1745a193956d67f7af0418aba776aa
Justdwiwt <>
2018-09-23 09:57:44 +0800

    定义模型

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+# 定义信息发布表
+# content   保存消息的内容
+# user_name 保存发布人的名字
+# kind      保存消息的类型
+class Moment(models.Model):
+    content = models.CharField(max_length=200)
+    user_name = models.CharField(max_length=20)
+    kind = models.CharField(max_length=20)
